87b779610a21a35ee5c98811849e7251a3cb084d
Ehabmd93 <ehabmd93@gmail.com>
2024-07-31 02:02:08 +0930

    Update app.py

--- app.py
+++ app.py
@@ -335,12 +335,12 @@ def generate_interactive_graph(data):
 
         time_ticks = pd.date_range(start=start_time, end=data['TIMESTAMP'].max(), freq='15T')
            fig.update_xaxes(
-            tickvals=[(t - start_time).total_seconds() / 60 for t in time_ticks],
-            ticktext=[t.strftime('%H:%M') for t in time_ticks],
-            title='Time Elapsed (Minutes)'
-        )
+        tickvals=[(t - start_time).total_seconds() / 60 for t in time_ticks],
+        ticktext=[t.strftime('%H:%M') for t in time_ticks],
+        title='Time Elapsed (Minutes)'
+    )
 
-          fig.update_layout(
+    fig.update_layout(
         title='Flow Rate and Effective Pressure vs Time for Mixes',
         yaxis=dict(title='Flow Rate (L/min)', side='left'),
         yaxis2=dict(title='Effective Pressure (bar)', overlaying='y', side='right'),
